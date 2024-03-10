327a2e4b5c8dc381f0817a3486636850569fe614
Eseosa Omo Enabu Owen <omobill2000@gmail.com>
2024-03-10 23:52:10 +0100

    Written codes for adding new posts

--- App.js
+++ App.js
@@ -1,26 +1,49 @@
 import {
   ActivityIndicator,
+  Alert,
+  Button,
   FlatList,
+  Pressable,
   SafeAreaView,
   StatusBar,
   Text,
+  TextInput,
   View,
 } from "react-native";
 import {
   QueryClientProvider,
   QueryClient,
   useInfiniteQuery,
+  useMutation,
 } from "react-query";
 import axios from "axios";
 import styled from "styled-components";
+import { useState } from "react";
 
 function NetworkingDemo() {
   // TODO Here we want to display 10 posts first, then if the user refreshes another 10 posts, and so on.
+  // ! State variables
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [error, setError] = useState("");
+
   async function retrievePosts({ pageParam = 1 }) {
     // NB Here, we need to make sure we are fetching a limit of 10 and the pageParam will be based on the getNextPageParam of the useInfiniteQueries.
     try {
       const response = await axios.get(
         `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageParam}`
+      );
+      return response.data;
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
+
+  async function addPosts(fieldsData) {
+    try {
+      const response = await axios.post(
+        "https://jsonplaceholder.typicode.com/posts",
+        fieldsData
       );
       return response.data;
     } catch (ex) {
@@ -42,6 +65,31 @@ function NetworkingDemo() {
       },
     });
 
+  const { mutate: addPostMutate } = useMutation(addPosts, {
+    onSuccess: () => Alert.alert("Post added"),
+  });
+
+  function validateForm() {
+    let error = "";
+    if (!title || !body) error = "Title and Body must be filled";
+    setError(error);
+
+    // ! returns true if no error
+    return !error;
+  }
+
+  function handleSubmit() {
+    if (validateForm()) {
+      addPostMutate({
+        title,
+        body,
+      });
+      setTitle("");
+      setBody("");
+      setError("");
+    }
+  }
+
   if (isLoading) {
     return (
       <LoadingContainer>
@@ -53,19 +101,43 @@ function NetworkingDemo() {
 
   return (
     <Container>
+      <AddPostContainer>
+        <AddPostHeader>Add Post</AddPostHeader>
+        <InputField placeholder="Title" value={title} onChangeText={setTitle} />
+        <InputField
+          placeholder="Body"
+          value={body}
+          onChangeText={setBody}
+          multiline
+          style={{ height: 70, textAlignVertical: "top" }}
+        />
+        <PostButton onPress={handleSubmit}>
+          <Text style={{ fontSize: 17, textAlign: "center" }}>Post</Text>
+        </PostButton>
+      </AddPostContainer>
       <ListContainer>
         <FlatList
           // ? So now, when it comes to InfiniteQueries the data is an object that contains pages, pages is an array that contains a page, and each page is an array that contains posts.
           // * So first we need to pass down the pages to the data prop
           // * With the way FlatList is built, it loops through the data prop for us and would give us a single page, which is an array.
           data={data.pages}
-          renderItem={({ item: page }) => {
+          renderItem={({ item: page, index }) => {
             // NB So the next thing, we need to do is loop through that array, so basically the renderItem will return just a page, then we loop through the page inside the renderItem and display each posts for that page.
             return (
-              <View style={{ borderWidth: 2 }}>
+              // ! To make sure that the last page is the one with the extra space at the bottom, we need to find out what page we are on and then compare it with the length of the page.
+              <View
+                style={{
+                  [index + 1 === data.pages.length && "marginBottom"]: 15,
+                }}
+              >
                 {page &&
                   page.map((post) => (
-                    <Post key={post.id} style={{ marginBottom: 10 }}>
+                    <Post
+                      key={post.id}
+                      style={{
+                        marginBottom: 15,
+                      }}
+                    >
                       <TitleText style={{ fontWeight: "bold", fontSize: 18 }}>
                         {post.title}
                       </TitleText>
@@ -78,7 +150,7 @@ function NetworkingDemo() {
           style={{ padding: 10 }}
           ListHeaderComponent={() => <ListHeader>JSON LIST</ListHeader>}
           ListEmptyComponent={() => <NoItemsFound>No Posts found</NoItemsFound>}
-          ItemSeparatorComponent={() => <View style={{ height: 10 }}></View>}
+          ItemSeparatorComponent={() => <View style={{ height: 5 }}></View>}
           refreshing={isFetching}
           onRefresh={fetchNextPage}
         />
@@ -101,6 +173,34 @@ const Container = styled(SafeAreaView)`
   flex: 1;
   background-color: #f5f5f5f5;
   padding-top: ${StatusBar.currentHeight}px; // ! android
+`;
+
+const AddPostContainer = styled(View)`
+  padding: 0 16px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
+
+const AddPostHeader = styled(Text)`
+  font-size: 22px;
+  font-weight: bold;
+  margin-bottom: 16px;
+  text-align: center;
+`;
+
+const InputField = styled(TextInput)`
+  border: solid 1px black;
+  border-radius: 6px;
+  padding: 5px;
+  height: 40px;
+  margin-bottom: 16px;
+`;
+
+const PostButton = styled(Pressable)`
+  width: 100%;
+  background-color: #b2a3f1;
+  padding: 10px;
+  border-radius: 6px;
 `;
 
 const LoadingIndicator = styled(ActivityIndicator)`
@@ -134,7 +234,7 @@ const ListHeader = styled(Text)`
 const Post = styled(View)`
   padding: 16px;
   width: 100%;
-  background-color: #ccc4ca;
+  background-color: #b2a3f1;
   border-radius: 7px;
 `;
 
